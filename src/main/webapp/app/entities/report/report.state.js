65857110a6c70dab326d5b1adce5c4b0bbcd1bc6
thoughtwizardssolutions <thoughtwizardssolutions@gmail.com>
2016-06-23 09:43:02 +0530

    Updated

--- src/main/webapp/app/entities/report/report.state.js
+++ src/main/webapp/app/entities/report/report.state.js
@@ -9,23 +9,43 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
-        .state('report', {
+        .state('reports', {
             parent: 'entity',
-            url: '/report',
+            url: '/reports?page&sort&search',
             data: {
-                authorities: ['ROLE_USER','ROLE_ORG_ADMIN','ROLE_ADMIN'],
+                authorities: ['ROLE_USER', 'ROLE_ORG_ADMIN'],
                 pageTitle: 'Reports'
             },
             views: {
                 'content@': {
-                    templateUrl: 'app/entities/report/reports.html',
-                    controller: 'ReportController',
+                    templateUrl: 'app/entities/reports/reports.html',
+                    controller: 'ReportsController',
                     controllerAs: 'vm'
                 }
             },
+            params: {
+                page: {
+                    value: '1',
+                    squash: true
+                },
+                sort: {
+                    value: 'id,asc',
+                    squash: true
+                },
+                search: null
+            },
             resolve: {
+                pagingParams: ['$stateParams', 'PaginationUtil', function ($stateParams, PaginationUtil) {
+                    return {
+                        page: PaginationUtil.parsePage($stateParams.page),
+                        sort: $stateParams.sort,
+                        predicate: PaginationUtil.parsePredicate($stateParams.sort),
+                        ascending: PaginationUtil.parseAscending($stateParams.sort),
+                        search: $stateParams.search
+                    };
+                }]
             }
-        });
+        })
     }
 
 })();
